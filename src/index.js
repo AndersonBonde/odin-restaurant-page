8e008136dfbd16f138c801c29c16955305397be6
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-20 12:05:08 -0300

    Create homepage structure from JavaScript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import pageLoad from "./pageLoad.js";
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
 
 pageLoad();
 
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -4,6 +4,7 @@ export default function pageLoad() {
     let content = document.querySelector(".content");
 
     loadBackground(content);
+    loadContainer(content);
 }
 
 function loadBackground(target) {
@@ -12,4 +13,68 @@ function loadBackground(target) {
     bgImage.classList.add("background")
 
     target.appendChild(bgImage);
+}
+
+function loadContainer(target) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    target.appendChild(container);
+
+    addTitle(container);
+    addAboutUsSection(container);
+    addOpenAtSection(container);
+}
+
+function addTitle(target) {
+    const title = document.createElement("h1");
+    const icon = document.createElement("i");
+
+    title.textContent = "Three Coffees Kiosk ";
+    icon.classList.add("fa-solid", "fa-mug-saucer");
+
+    title.appendChild(icon);
+    target.appendChild(title);
+}
+
+function addAboutUsSection(target) {
+    const section = document.createElement("section");
+    const header = document.createElement("header");
+    const para = document.createElement("p");
+
+    header.textContent = "About Us";
+    para.textContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium soluta tempore est ipsum optio voluptas officiis perferendis magnam, voluptatibus veniam iusto? Natus, vitae doloremque mollitia accusantium neque officia aut molestias est earum.";
+
+    section.appendChild(header);
+    section.appendChild(para);
+    target.appendChild(section);
+}
+
+function addOpenAtSection(target) {
+    const section = document.createElement("section");
+    const header = document.createElement("header");
+
+    header.textContent = "Open At";
+
+    section.appendChild(header);
+    target.appendChild(section);
+
+    addOpenDays(section);
+}
+
+function addOpenDays(target) {
+    const ul = document.createElement("ul");
+    const weekObj = {
+        day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        message: ["Closed", "8am - 16pm", "8am - 16pm", "8am - 16pm", "8am - 16pm", "8am - 16pm", "8am - 16pm"],
+    }
+
+    weekObj.day.forEach((_, index) => {
+        const li = document.createElement("li");
+        li.textContent = weekObj.day[index] + ": " + weekObj.message[index];
+
+        ul.appendChild(li);
+    });
+
+    target.appendChild(ul);
 }
