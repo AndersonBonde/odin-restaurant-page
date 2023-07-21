af9062079e85288aa1be70ec21b791243a992bb6
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-21 11:43:16 -0300

    Add functionality to switch pages(Home & Menu)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import menuLoad from "./menuLoad.js";
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 
-// pageLoad();
-menuLoad();
+const homeLink = document.querySelector("#home-link");
+const menuLink = document.querySelector("#menu-link");
 
+homeLink.addEventListener("click", loadHomePage);
+menuLink.addEventListener("click", loadMenuPage);
+
+function loadHomePage() {
+    unloadAll();
+    pageLoad();
+}
+loadHomePage();
+
+function loadMenuPage() {
+    unloadAll();
+    menuLoad();    
+}
+
+function unloadAll() {
+    const content = document.querySelector(".content");
+    content.innerHTML = "";    
+}
